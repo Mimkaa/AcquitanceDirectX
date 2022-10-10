da6a066723c406857c34b5a29dd22542f30a7cee
Illia Solohub <you@youraddress.com>
2022-10-10 23:11:44 +0300

    indexed drawing

--- AcquitanceDirectX/Graphics.cpp
+++ AcquitanceDirectX/Graphics.cpp
@@ -132,6 +132,9 @@ void Graphics::DrawTriangle()
         { 0.0f, 0.5f, 255, 0, 0, 0 },
         { 0.5f, -0.5f, 0, 255, 0, 0  },
         { -0.5f, -0.5f, 0, 0, 255, 0  },
+        {-0.3, 0.3f,0,255,0,0},
+        {0.3f,0.3,0,0,255,0},
+        {0.0f,-0.8f,255,0,0,0},
         
         
         
@@ -155,7 +158,29 @@ void Graphics::DrawTriangle()
     UINT stride = sizeof(Vertex);
     UINT offset = 0u;
     pContext->IASetVertexBuffers(0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset);
- 
+
+    // create index buffer
+    const unsigned short indices[] =
+    {
+        0,1,2,
+        0,2,3,
+        0,4,1,
+        2,1,5,
+    };
+    wrl::ComPtr<ID3D11Buffer> pIndexBuffer;
+    D3D11_BUFFER_DESC ibd = { 0 };
+    ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+    ibd.Usage = D3D11_USAGE_DEFAULT;
+    ibd.CPUAccessFlags = 0u;
+    ibd.MiscFlags = 0u;
+    ibd.ByteWidth = sizeof(indices);
+    ibd.StructureByteStride = sizeof(const unsigned short);
+    D3D11_SUBRESOURCE_DATA isb = { 0 };
+    isb.pSysMem = indices;
+    GFX_THROW_INFO(pDevice->CreateBuffer(&ibd, &isb, &pIndexBuffer));
+    // bind index buffer
+    pContext->IASetIndexBuffer(pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0u);
+   
    
     // create pixel shader
     wrl::ComPtr<ID3D11PixelShader> pPixelShader;
@@ -207,7 +232,7 @@ void Graphics::DrawTriangle()
     vp.TopLeftY = 0;
     pContext->RSSetViewports(1u, &vp);
     
-    GFX_THROW_INFO_ONLY(pContext->Draw((UINT)std::size(vertices), 0u));
+    GFX_THROW_INFO_ONLY(pContext->DrawIndexed((UINT)std::size(indices), 0u,0u));
 }
 
 Graphics::HrException::HrException(int line, const char* file, HRESULT hr, std::vector<std::string> infoMsgs ) noexcept
