e373c654834532cff53d2e815d590537c3812700
Illia Solohub <you@youraddress.com>
2022-10-13 15:02:17 +0300

    decoupling of common(static) binds and spesific for each box binds

--- AcquitanceDirectX/Drawable.h
+++ AcquitanceDirectX/Drawable.h
@@ -2,10 +2,13 @@
 #include "Graphics.hpp"
 #include <DirectXMath.h>
 #include <memory>
+
 class Bindable;
 
 class Drawable
 {
+	template<class T>
+	friend class DrawableBase;
 public:
 	Drawable() = default;
 	Drawable(const Drawable&) = delete;
@@ -15,6 +18,8 @@ public:
 	void AddBind(std::unique_ptr<Bindable> bind) noexcept(!IS_DEBUG);
 	void AddIndexBuffer(std::unique_ptr<class IndexBuffer> ibuf) noexcept;
 	virtual ~Drawable() = default;
+private:
+	virtual const std::vector<std::unique_ptr<Bindable>>& GetStaticBinds() const noexcept = 0;
 private:
 	const IndexBuffer* pIndexBuffer = nullptr;
 	std::vector<std::unique_ptr<Bindable>> binds;
