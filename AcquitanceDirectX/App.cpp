39d5b64f7333d5446bd74d331d9c16955c61ba40
Illia Solohub <you@youraddress.com>
2022-10-14 20:07:49 +0300

    real bindable/drawable test + GDI+ connection

--- AcquitanceDirectX/App.cpp
+++ AcquitanceDirectX/App.cpp
@@ -1,22 +1,68 @@
 #include "App.hpp"
+#include "Melon.h"
+#include "Pyramid.h"
 #include "Box.h"
 #include <memory>
+#include <algorithm>
+#include "ChiliMath.h"
+#include "GDIPlusManager.h"
+#include "Surface.h"
+GDIPlusManager gdipm;
+
 
 App::App() :wnd(800, 600, L"I understood how it works)))")
 {
-	std::mt19937 rng(std::random_device{}());
-	std::uniform_real_distribution<float> adist(0.0f, 3.1415f * 2.0f);
-	std::uniform_real_distribution<float> ddist(0.0f, 3.1415f * 2.0f);
-	std::uniform_real_distribution<float> odist(0.0f, 3.1415f * 0.3f);
-	std::uniform_real_distribution<float> rdist(6.0f, 20.0f);
-	for (auto i = 0; i < 80; i++)
+	class Factory
 	{
-		boxes.push_back(std::make_unique<Box>(
-			wnd.Gfx(), rng, adist,
-			ddist, odist, rdist
-			));
-	}
-	wnd.Gfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 40.0f));
+	public:
+		Factory(Graphics & gfx)
+			:
+			gfx(gfx)
+		{}
+		std::unique_ptr<Drawable> operator()()
+		{
+			switch (typedist(rng))
+			{
+			case 0:
+				return std::make_unique<Pyramid>(
+					gfx, rng, adist, ddist,
+					odist, rdist
+					);
+			case 1:
+				return std::make_unique<Box>(
+					gfx, rng, adist, ddist,
+					odist, rdist, bdist
+					);
+			case 2:
+				return std::make_unique<Melon>(
+					gfx, rng, adist, ddist,
+					odist, rdist, longdist, latdist
+					);
+			default:
+				assert(false && "bad drawable type in factory");
+				return {};
+			}
+		}
+	private:
+		Graphics& gfx;
+		std::mt19937 rng{ std::random_device{}() };
+		std::uniform_real_distribution<float> adist{ 0.0f,PI * 2.0f };
+		std::uniform_real_distribution<float> ddist{ 0.0f,PI * 0.5f };
+		std::uniform_real_distribution<float> odist{ 0.0f,PI * 0.08f };
+		std::uniform_real_distribution<float> rdist{ 6.0f,20.0f };
+		std::uniform_real_distribution<float> bdist{ 0.4f,3.0f };
+		std::uniform_int_distribution<int> latdist{ 5,20 };
+		std::uniform_int_distribution<int> longdist{ 10,40 };
+		std::uniform_int_distribution<int> typedist{ 0,2 };
+};
+
+Factory f(wnd.Gfx());
+drawables.reserve(nDrawables);
+std::generate_n(std::back_inserter(drawables), nDrawables, f);
+
+const auto s = Surface::FromFile("Images\\uta.png");
+
+wnd.Gfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 40.0f));
 }
 
 App::~App()
@@ -41,7 +87,7 @@ void App::DoFrame()
 {
 	auto dt = timer.Mark();
 	wnd.Gfx().ClearBuffer(0.07f, 0.0f, 0.12f);
-	for (auto& b : boxes)
+	for (auto& b : drawables)
 	{
 		b->Update(dt);
 		b->Draw(wnd.Gfx());
--- AcquitanceDirectX/Box.cpp
+++ AcquitanceDirectX/Box.cpp
@@ -1,12 +1,13 @@
 #include "Box.h"
 #include "BindableBase.h"
 #include "GraphicsThrowHeader.h"
-#include "Sphere.h"
+#include "Cube.h"
 Box::Box(Graphics& gfx, std::mt19937& rng,
 	std::uniform_real_distribution<float>& adist,
 	std::uniform_real_distribution<float>& ddist,
 	std::uniform_real_distribution<float>& odist,
-	std::uniform_real_distribution<float>& rdist)
+	std::uniform_real_distribution<float>& rdist,
+	std::uniform_real_distribution<float>& bdist)
 	:
 	r(rdist(rng)),
 	droll(ddist(rng)),
@@ -22,26 +23,27 @@ Box::Box(Graphics& gfx, std::mt19937& rng,
 	if (!IsStaticInitialized())
 	{
 		namespace dx = DirectX;
+
 		struct Vertex
 		{
 			dx::XMFLOAT3 pos;
 		};
 		
-		auto model = Sphere::Make<Vertex>();
+		auto model = Cube::Make<Vertex>();
 		model.Transform(dx::XMMatrixScaling(1.0f, 1.0f, 1.2f));
 
 		AddStaticBind(std::make_unique<VertexBuffer>(gfx, model.vertices));
 
 
-		auto pvs = std::make_unique<VertexShader>(gfx, L"VertexShader.cso");
+		auto pvs = std::make_unique<VertexShader>(gfx, L"ColorIndexVS.cso");
 		auto pvsbc = pvs->GetBytecode();
 		AddStaticBind(std::move(pvs));
 
-		AddStaticBind(std::make_unique<PixelShader>(gfx, L"PixelShader.cso"));
+		AddStaticBind(std::make_unique<PixelShader>(gfx, L"ColorIndexPS.cso"));
 
 		AddStaticIndexBuffer(std::make_unique<IndexBuffer>(gfx, model.indices));
 
-		struct ConstantBuffer2
+		struct PixelShaderConstants
 		{
 			struct
 			{
@@ -49,20 +51,22 @@ Box::Box(Graphics& gfx, std::mt19937& rng,
 				float g;
 				float b;
 				float a;
-			} face_colors[6];
+			} face_colors[8];
 		};
-		const ConstantBuffer2 cb2
+		const PixelShaderConstants cb2
 		{
 			{
+				{1.0,1.0,1.0},
 				{1.0f, 0.0f, 1.0f},
 				{1.0f, 0.0f, 0.0f},
 				{0.0f, 1.0f, 0.0f},
 				{0.0f, 0.0f, 1.0f},
 				{1.0f, 1.0f, 0.0f},
-				{0.0f, 1.0f, 1.0f}
+				{0.0f, 1.0f, 1.0f},
+				{0.0,0.0,0.0},
 			}
 		};
-		AddStaticBind(std::make_unique<PixelConstantBuffer<ConstantBuffer2>>(gfx, cb2));
+		AddStaticBind(std::make_unique<PixelConstantBuffer<PixelShaderConstants>>(gfx, cb2));
 		const std::vector<D3D11_INPUT_ELEMENT_DESC> ied =
 		{
 			{"Position",0,DXGI_FORMAT_R32G32B32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0}
@@ -75,6 +79,14 @@ Box::Box(Graphics& gfx, std::mt19937& rng,
 		SetIndexFromStatic();
 	}
 	AddBind(std::make_unique<TransformCbuf>(gfx, *this));
+
+	// model deformation transform (per instance, not stored as bind)
+	DirectX::XMStoreFloat3x3(
+		&mt,
+		DirectX::XMMatrixScaling(1.0f, 1.0f, bdist(rng))
+	);
+
+
 }
 
 void Box::Update(float dt) noexcept
@@ -89,8 +101,10 @@ void Box::Update(float dt) noexcept
 
 DirectX::XMMATRIX Box::GetTransformXM() const noexcept
 {
-	return DirectX::XMMatrixRotationRollPitchYaw(pitch, yaw, roll) *
-		DirectX::XMMatrixTranslation(r, 0.0f, 0.0f) *
-		DirectX::XMMatrixRotationRollPitchYaw(theta, phi, chi) *
-		DirectX::XMMatrixTranslation(0.0f, 0.0f, 20.0f);
+	namespace dx = DirectX;
+	return dx::XMLoadFloat3x3(&mt) *
+		dx::XMMatrixRotationRollPitchYaw(pitch, yaw, roll) *
+		dx::XMMatrixTranslation(r, 0.0f, 0.0f) *
+		dx::XMMatrixRotationRollPitchYaw(theta, phi, chi) *
+		dx::XMMatrixTranslation(0.0f, 0.0f, 20.0f);
 }
